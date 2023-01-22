2e03c629b55e8f3abd7c47e24e6866d40254f44e
albuska <109683293+albuska@users.noreply.github.com>
2023-01-22 18:12:29 +0200

    finish 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,37 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const containerRef = document.querySelector('.gallery');
+
+containerRef.addEventListener('click', onTargetItemClick); 
+
+const imagesList = createGalleryItem(galleryItems);
+
+function onTargetItemClick(event) {
+    event.preventDefault();
+if(event.target.nodeName !== 'IMG') {
+    return; 
+ }
+}
+
+function createGalleryItem(items) {
+    return items.map(item => 
+    `<a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+    </a>`).join('');
+    }
+
+containerRef.innerHTML = imagesList;
+
+    let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+});
+
