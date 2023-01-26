46e4d6aed58572fef2ae37322af3e9172637d50b
albuska <109683293+albuska@users.noreply.github.com>
2023-01-26 22:37:56 +0200

    complete 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,45 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'; 
 
+const iframeRef = document.getElementById('vimeo-player');
+
+const player = new Player(iframeRef);   
+
+player.on('timeupdate', throttle(data => {
+    localStorage.setItem("videoplayer-current-time", data.seconds)   
+}, 1000)
+)
+const currentTime = Number(localStorage.getItem("videoplayer-current-time"));
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+  
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+          
+            break;
+        default:
+     
+            break;
+    }
+});
+
+// 2 СПОСІБ
+// const VIDEOPLAYER_CURRENT_TIME = "videoplayer-current-time";
+
+// player.on(
+//     'timeupdate',
+//     throttle(data => {
+//       localStorage.setItem(
+//         VIDEOPLAYER_CURRENT_TIME,
+//         JSON.stringify(data.seconds)
+//       );
+//     }, 1000)
+//   );
+
+// 3 СПОСІБ
+// const onPlay = function(data) {
+//     localStorage.setItem("videoplayer-current-time", data.seconds);
+// }
+   
+// player.on('timeupdate', throttle(onPlay, 1000));  
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+const formRef = document.querySelector('.feedback-form'); 
+console.log("🚀 formRef", formRef);
 
+const inputRef = document.querySelector('.feedback-form input');
+console.log("🚀 inputRef", inputRef); 
+
+// const textareaRef = document.querySelector('.feedback-form textarea');
+// console.log("🚀 textareaRef", textareaRef);
+
+// const buttonRef = document.querySelector('.feedback-form button');
+// console.log("🚀 buttonRef", buttonRef); 
+
+formRef.addEventListener('input', getFieldsForm); 
+
+const FEEDBACK_FORM_STATE = "feedback-form-state"; 
+
+function getFieldsForm(event) {
+// event.preventDefault(); 
+console.log(event.elements.message.value); 
+// localStorage.setItem(FEEDBACK_FORM_STATE, formRef.elements.message.value);
+}
+
+const object = {
+    input: email,
+    textarea: message, 
+}
+console.log(object); 
+
+function getFieldsForm(event) {
+            if(event.target.name === 'email') {
+  object.input = event.target.value; 
+            } else if(event.target.name === 'message')
+   object.textarea = event.target.value; 
+        }
+
+// function getFieldsForm(event) {
+//         if(event.target.name === 'email') {
+//     localStorage.setItem(FEEDBACK_FORM_STATE, event.target.value); 
+//         } else if(event.target.name === 'message')
+//     localStorage.setItem(FEEDBACK_FORM_STATE, event.target.value); 
+//     }
+
+
+// buttonRef.addEventListener('submit', event => {
+//     console.log('Форма відправлена', event);
+// }); 
